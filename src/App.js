01d7ae3dc42bb01c399fc22c0ea686ecee4254fc
Promit Saha <promitsaha154@gmail.com>
2023-12-31 14:08:09 +0530

    Refactor code to use consistent quotation marks and spacing

--- src/App.js
+++ src/App.js
@@ -1,39 +1,98 @@
-import React, { useState, useEffect } from 'react';
-import ProfileForm from './ProfileForm';
-import ProfileList from './ProfileList';
+import React, { useState, useEffect } from "react";
+import ProfileForm from "./ProfileForm";
+import ProfileList from "./ProfileList";
 
 function App() {
-    const [profiles, setProfiles] = useState([]);
+  const [profiles, setProfiles] = useState([]);
+  const [showDeleteSuccess, setShowDeleteSuccess] = useState(false);
 
-    useEffect(() => {
-        // Fetch existing profiles
-        fetch('https://backend-demo-3hll.onrender.com/profiles')
-            .then((response) => response.json())
-            .then(setProfiles)
-            .catch(console.error);
-    }, []);
+  useEffect(() => {
+    fetchProfiles();
+  }, []);
 
-    const addProfile = (profile) => {
-        // Send the new profile to the server
-        fetch('https://backend-demo-3hll.onrender.com/profiles', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(profile),
+  const fetchProfiles = () => {
+    fetch("https://backend-demo-3hll.onrender.com/profiles")
+      .then((response) => response.json())
+      .then((data) => setProfiles(data))
+      .catch(console.error);
+  };
+
+  const addProfile = (profile) => {
+    fetch("https://backend-demo-3hll.onrender.com/profiles", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(profile),
+    })
+      .then((response) => response.json())
+      .then((newProfile) => setProfiles([...profiles, newProfile]))
+      .catch(console.error);
+  };
+
+  const deleteProfile = (id) => {
+    setTimeout(() => {
+      fetch(`https://backend-demo-3hll.onrender.com/profiles/${id}`, {
+        method: "DELETE",
+      })
+        .then(() => {
+          setProfiles(profiles.filter((profile) => profile._id !== id));
+          setShowDeleteSuccess(true); // Show success message
+          setTimeout(() => setShowDeleteSuccess(false), 5000); // Hide after 3 seconds
         })
-            .then((response) => response.json())
-            .then((newProfile) => setProfiles([...profiles, newProfile]))
-            .catch(console.error);
-    };
+        .catch(console.error);
+    }, 500);
+  };
 
-    return (
-        <div className="container mx-auto p-5">
-            <h1 className="text-gray-800 text-3xl font-bold mb-6 text-center">Profile Submissions</h1>
-            <ProfileForm onSubmit={addProfile} />
-            <ProfileList profiles={profiles} />
+  return (
+    <div className="container mx-auto p-5">
+      <h1 className="text-gray-800 text-3xl font-bold mb-6 text-center">
+        Profile Submissions
+      </h1>
+      {showDeleteSuccess && (
+        <div className="mt-12 mx-4 px-4 rounded-md border-l-4 border-red-500 bg-red-50 md:max-w-2xl md:mx-auto md:px-8">
+          <div className="flex justify-between py-3">
+            <div className="flex">
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-red-500"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="self-center ml-3">
+                <span className="text-red-600 font-semibold">Error</span>
+                <p className="text-red-600 mt-1">
+                  Profile has been deleted successfully!
+                </p>
+              </div>
+            </div>
+            <button className="self-start text-red-500">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
-    );
+      )}
+      <ProfileForm onSubmit={addProfile} />
+      <ProfileList profiles={profiles} onDelete={deleteProfile} />
+    </div>
+  );
 }
 
 export default App;
--- src/ProfileList.js
+++ src/ProfileList.js
@@ -1,20 +1,26 @@
 import React from 'react';
 
-function ProfileList({ profiles }) {
+function ProfileList({ profiles, onDelete }) {
     return (
         <div className="container mx-auto mt-10">
             <ul className="list-none">
-                {profiles.map((profile, index) => (
-                    <li key={index} className="mb-4 p-4 border border-gray-200 rounded shadow-lg">
-                        <p className="text-xl font-semibold">{profile.name}</p>
-                        <div className="flex space-x-4 mt-2">
-                            <a href={profile.twitterLink} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700 transition duration-300">
-                                Twitter
-                            </a>
-                            <a href={profile.githubLink} target="_blank" rel="noopener noreferrer" className="text-gray-800 hover:text-gray-600 transition duration-300">
-                                GitHub
-                            </a>
+                {profiles.map((profile) => (
+                    <li key={profile._id} className="mb-4 p-4 border border-gray-200 rounded shadow-lg flex justify-between items-center">
+                        <div>
+                            <p className="text-xl font-semibold">{profile.name}</p>
+                            <div className="flex space-x-4 mt-2">
+                                <a href={profile.twitterLink} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700 transition duration-300">
+                                    Twitter
+                                </a>
+                                <a href={profile.githubLink} target="_blank" rel="noopener noreferrer" className="text-gray-800 hover:text-gray-600 transition duration-300">
+                                    GitHub
+                                </a>
+                            </div>
                         </div>
+                        <button onClick={() => onDelete(profile._id)} className="text-red-500 hover:text-red-700">
+                            {/* Place your red cross icon here */}
+                            ❌
+                        </button>
                     </li>
                 ))}
             </ul>
