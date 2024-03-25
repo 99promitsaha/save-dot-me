4a40ca76abb28fc464e0e07762c53e577b67ec1d
Promit Saha <promitsaha154@gmail.com>
2024-03-25 19:01:15 +0530

    Update Header component with deprecation message

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,15 @@ import React from "react";
 
 function Header () {
     return (
-        <div className="bg-blue-600 mb-10">
+        <div className="bg-green-600 mb-10">
         <div className="max-w-screen-xl mx-auto px-4 py-3 flex items-start justify-between text-white sm:items-center md:px-8">
           <div className="flex-1 justify-center flex items-start gap-x-4 sm:items-center">
             <div className="flex-none p-1.5 px-4 rounded-full bg-fuchsia-50 text-zinc-800 flex items-center justify-center font-medium text-sm">
               Alert 🔔
             </div>
             <p className="font-medium p-2">
-              This tool, open to all, tracking your favorite
-              personalities on Twitter.{" "}
+              This too, has been deprecated.
+              The API's no longer work.{" "}
               <a
                 href="https://twitter.com/99promitsaha"
                 target="_blank"
